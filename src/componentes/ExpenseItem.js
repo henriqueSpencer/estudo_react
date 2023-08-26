c71171210a9b7847c8eac1d042e7e4268a9e8910
henriqueSpencer <henriquespencer11@gmail.com>
2023-08-26 17:14:50 -0300

    add

--- src/componentes/ExpenseItem.js
+++ src/componentes/ExpenseItem.js
@@ -1,14 +1,18 @@
 import "./ExpenseItem.css";
 
 // Sempre deixar o nome da funcao em maiusculo,
-//pq ai a tag tbm sera em maiusculo se diferenciando de uma tag html normal
+// pq ai a tag tbm sera em maiusculo se diferenciando de uma tag html normal
 function ExpenseItem() {
+  const gastoData = new Date(2021, 2, 28);
+  const gastoTitle = "Seguro Carro";
+  const gastoValor = 332.22;
+
   return (
-    <div className="expanse-item">
-      <div>Março 28 2023</div>
-      <div className="expense-item__desription">
-        <h2>Seguro Carro</h2>
-        <div className="expense-item__price">R$294.67</div>
+    <div className="expense-item">
+      <div>{gastoData.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{gastoTitle}</h2>
+        <div className="expense-item__price">R${Math.random()}</div>
       </div>
     </div>
   );
