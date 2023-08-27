456e327eaebd0cf876971a85a715310f65f6f912
henriqueSpencer <henriquespencer11@gmail.com>
2023-08-26 21:28:15 -0300

    passing as props values

--- src/App.js
+++ src/App.js
@@ -3,28 +3,35 @@ import "./App.css";
 import ExpenseItem from "./componentes/ExpenseItem";
 
 function App() {
+  const despesas = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
-    <ExpenseItem></ExpenseItem>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //     <p>
-    //       Feito por Henrique, Vlw Playboy 😎
-    //     </p>
-    //     <ExpenseItem></ExpenseItem>
-    //   </header>
-    // </div>
+    <div>
+      {/* <ExpenseItem titulo={despesas[0].title}></ExpenseItem> */}
+      {/* <ExpenseItem props={despesas[0]}></ExpenseItem> */}
+      <ExpenseItem props={despesas[0]}></ExpenseItem>
+      <ExpenseItem props={despesas[2]}></ExpenseItem>
+    </div>
   );
 }
 
--- src/componentes/ExpenseItem.js
+++ src/componentes/ExpenseItem.js
@@ -2,17 +2,18 @@ import "./ExpenseItem.css";
 
 // Sempre deixar o nome da funcao em maiusculo,
 // pq ai a tag tbm sera em maiusculo se diferenciando de uma tag html normal
-function ExpenseItem() {
-  const gastoData = new Date(2021, 2, 28);
-  const gastoTitle = "Seguro Carro";
-  const gastoValor = 332.22;
+function ExpenseItem(props) {
+  // const gastoData = new Date(2021, 2, 28);
+  // const gastoTitle = "Seguro Carro";
+  // const gastoValor = 332.22;
 
   return (
     <div className="expense-item">
-      <div>{gastoData.toISOString()}</div>
+      <div>{props.props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{gastoTitle}</h2>
-        <div className="expense-item__price">R${Math.random()}</div>
+        <h2>{props.props.title}</h2>
+        {console.log(props.props.title)}
+        <div className="expense-item__price">R${props.props.amount}</div>
       </div>
     </div>
   );
