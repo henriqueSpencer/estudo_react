7f58570080a550150a4bf482eed23a6de06f322a
henriqueSpencer <henriquespencer11@gmail.com>
2023-09-03 20:00:44 -0300

    Add(Form)

--- src/componentes/NewExpense/ExpenseForm.js
+++ src/componentes/NewExpense/ExpenseForm.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const titleChangeHandler = () => {
-    console.log("Titulo Mudou");
+  //FORMA 00 DE ATUALIZAR CAMPO, UM POR UM
+  // const [enteredTitle, setEnteredTitle] = useState("");
+
+  // const titleChangeHandler = (event) => {
+  //   console.log(event.target.value);
+  //   setEnteredTitle(event.target.value);
+  // };
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    });
+  };
+
+  const subimitHandler = (event) => {
+    // Previnindo de dar reload ao dar subbimit
+    event.preventDefault();
+
+    const expenseDate = {
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: new Date(userInput.enteredDate),
+    };
+    console.log(expenseDate);
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: "" };
+    });
   };
 
   return (
-    <form>
+    <form onSubmit={subimitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Titulo</label>
-          <input type="text" onChange={titleChangeHandler}></input>
+          <input
+            type="text"
+            value={userInput.enteredTitle}
+            onChange={titleChangeHandler}
+          ></input>
+          {/* <input type="text" onChange={(event) => inputChangeHandle('title', event.target.value)}></input> */}
         </div>
       </div>
       <div className="new-expense__controls">
